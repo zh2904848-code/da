e4626387cfe1f8188e0d2693e9b760b141cd5e2e
zh2904848-code <zh2904848@gmail.com>
2026-07-15 04:36:02 +0800

    Align root background music volume

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let currentAudio = null;
 let actionAudio = null;
 let backgroundAudio = null;
 let backgroundStarted = false;
-const backgroundVolume = 0.35;
-const duckedBackgroundVolume = 0.12;
+const backgroundVolume = 0.05;
+const duckedBackgroundVolume = 0.015;
 
 const revealMap = {
   "bubble-xiaohong": {
